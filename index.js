9f5c9bc44cc47345c0af6859ff7beec5ba23bb47
connorgillis <connorgillis@Connors-MacBook-Pro.local>
2017-01-02 19:28:10 -0500

    Person Objects Created
    People objects created based on number of people, object name properly
    contains i int and ‘person’ string. Each obj then logged via console/

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 function genPeople() {
   var numPeople = document.getElementById("numPeople").value;
-  
+  parseInt(numPeople);
+  console.log(numPeople);
+  for (i = 0; i < numPeople; i++) {
+    var personNumber = i.toString();
+    var objectName = "Person" + personNumber;
+    var Person = {
+      index: objectName,
+      initials: "XX",
+      shareG: "XX",
+      shareR: "XX",
+    };
+    console.log(Person);
+  };
 }
